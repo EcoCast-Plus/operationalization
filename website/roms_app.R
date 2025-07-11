18ac41eb7ce976db9b86082e929511ebe98d8e48
joshcullen <joshcullen10@gmail.com>
2025-07-11 16:25:50 -0400

    Turned on cron job in GHA YAML for ROMS workflow; updated CMEMS Shiny app and embedded on webpage; also created ROMS Shiny app and embedded on webpage

--- website/roms_app.R
+++ website/roms_app.R
@@ -1,5 +1,5 @@
 
-# Shiny app to explore CMEMS predictions and covariates by date
+# Shiny app to explore ROMS predictions and covariates by date
 
 library(shiny)
 library(bslib)
@@ -22,8 +22,8 @@ library(cmocean)
 base_api <- "https://api.github.com/repos/joshcullen/CEG_operationalization/contents"
 
 # Folders
-pred_folder <- "model_prediction/TopPredatorWatch/rasters"
-env_folder <- "data_processing/TopPredatorWatch/rasters"
+pred_folder <- "model_prediction/ROMS/rasters"
+env_folder <- "data_processing/ROMS/rasters"
 
 # Function to get file listing from GitHub folder
 get_file_dates <- function(folder, pattern, strip_pattern, strip = TRUE) {
@@ -55,7 +55,7 @@ env_rasters <- get_file_dates(folder = env_folder,
                               pattern = "\\.tiff$",
                               strip_pattern = "_\\d{4}-\\d{2}-\\d{2}",
                               strip = TRUE) |> 
-  filter(!str_detect(name, "day|ugosa|vgosa"))  #remove unneeded variables
+  filter(!str_detect(name, "lunar"))  #remove unneeded variables
 
 # Combine both
 all_rasters <- bind_rows(pred_rasters, env_rasters)
@@ -68,7 +68,8 @@ valid_dates <- intersect(pred_rasters$date, env_rasters$date) |>
 
 # Define list of palette colors from {cmocean} to viz covars
 covar_pal_df <- data.frame(covar = unique(env_rasters$layer),
-                           pals = c("algae","tempo","algae","dense","matter","delta","thermal","amp"))
+                           pals = c("tempo","deep","curl","dense","delta","matter","thermal","amp",
+                                    "curl","delta","curl","delta"))
 
 
 
@@ -132,8 +133,7 @@ server <- function(input, output, session) {
     pred_rasters |> 
       filter(date == input$date) |> 
       pull(url) |> 
-      rast() #|> 
-      # project('EPSG:3857')  # Reproject so properly mapped by leaflet
+      rast()
   })
   
   covar_rast <- reactive({
@@ -142,18 +142,17 @@ server <- function(input, output, session) {
              layer == input$covar) |> 
       pull(url) |> 
       rast() |> 
-      # project('EPSG:3857') |>  # Reproject so properly mapped by leaflet
       crop(pred_rast())  #crop to match same extent as prediction
   })
   
-   
+  
   
   ## Map prediction layer
   
   # Create basemap for prediction
   output$pred_map <- renderLeaflet({
     leaflet() |> 
-      setView(lng = -130, lat = 30, zoom = 3) |> 
+      setView(lng = -125, lat = 40, zoom = 4) |> 
       addProviderTiles(provider = providers$CartoDB.DarkMatter, group = "CartoDB",
                        options = providerTileOptions(zIndex = -10)) |> 
       addProviderTiles(provider = providers$Esri.WorldImagery, group = "Satellite",
@@ -164,7 +163,7 @@ server <- function(input, output, session) {
                        baseGroups = c("CartoDB","Satellite","Bathymetry"),
                        overlayGroups = "HSI",
                        options = layersControlOptions(collapsed = TRUE, autoZIndex = FALSE))
-    })
+  })
   
   # Add reactive layer to map
   observe({
@@ -174,8 +173,8 @@ server <- function(input, output, session) {
     leafletProxy("pred_map") |> 
       clearControls() |> 
       clearImages() |>
-      addRasterImage(project(pred_rast(), 'EPSG:3857'), colors = pal, opacity = 0.8, group = "HSI", layerId = "HSI") |> 
-      addImageQuery(project(pred_rast(), 'EPSG:3857'), layerId = "HSI") |>  #add raster  query
+      addRasterImage(pred_rast(), colors = pal, opacity = 0.8, group = "HSI", layerId = "HSI") |> 
+      addImageQuery(pred_rast(), layerId = "HSI") |>  #add raster  query
       addLegend(title = "Habitat Suitability", position = "bottomright", pal = pal, values = values(pred_rast()))
   })
   
@@ -185,7 +184,7 @@ server <- function(input, output, session) {
   # Create basemap for covar layers
   output$covar_map <- renderLeaflet({
     leaflet() |>
-      setView(lng = -130, lat = 30, zoom = 3) |>
+      setView(lng = -125, lat = 40, zoom = 4) |>
       addProviderTiles(provider = providers$CartoDB.DarkMatter, group = "CartoDB",
                        options = providerTileOptions(zIndex = -10)) |>
       addProviderTiles(provider = providers$Esri.WorldImagery, group = "Satellite",
@@ -197,7 +196,7 @@ server <- function(input, output, session) {
                        overlayGroups = c("covar"),
                        options = layersControlOptions(collapsed = TRUE, autoZIndex = FALSE))
   })
-
+  
   
   # Add reactive layer to map
   outputOptions(output, "covar_map", suspendWhenHidden = FALSE)  #need for displaying points for map on load
@@ -216,8 +215,8 @@ server <- function(input, output, session) {
     leafletProxy("covar_map") |> 
       clearControls() |> 
       clearImages() |>
-      addRasterImage(x = project(covar_rast(), 'EPSG:3857'), colors = covar_pal, opacity = 1, group = "covar") |>
-      addImageQuery(project(covar_rast(), 'EPSG:3857'), group = "covar", layerId = "covar") |>  #add raster  query
+      addRasterImage(x = covar_rast(), colors = covar_pal, opacity = 1, group = "covar") |>
+      addImageQuery(covar_rast(), group = "covar", layerId = "covar") |>  #add raster  query
       addLegend(title = paste(input$covar), position = "bottomright", pal = covar_pal,
                 values = values(covar_rast()), layerId = "covar", group = "covar") 
   })
